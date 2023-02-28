0cea8ba084f46c48a6fb5cf661b06de6f0025b50
Lim David <david.lim.0101@gmail.com>
2023-02-28 17:03:51 +0100

    fix error

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,10 +8,11 @@ export default function About() {
     const service = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.";
     const security = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."; 
     const desktopSize = ' 1440w';
+    const mobileSize = ' 375w, ';
 
     return (
         <section className="about">
-            <img src={gustavoAlves} srcSet={gustavoAlesDesktop+ desktopSize} alt='Des montagnes avec ces arbres.' />
+            <img height="226" width="336" src={gustavoAlves} srcSet={gustavoAlves + mobileSize+ gustavoAlesDesktop+ desktopSize } sizes="(max-width: 375px) 375px" alt='Des montagnes avec ces arbres.' />
             <DropDown title="Fiabilité" description={reliability}/>
             <DropDown title="Respect" description={respect}/>
             <DropDown title="Service" description={service}/>
